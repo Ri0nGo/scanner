66e31c5eb480fe54668d136e016d0facec53057e
Ri0n <35928029+Ri0nGo@users.noreply.github.com>
2025-05-20 16:55:55 +0800

    Update scanner.go

--- scanner-cmd/scanner.go
+++ scanner-cmd/scanner.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// 扫描器
 type Scanner struct {
 	Type   string
 	Ports  []int
@@ -13,11 +14,13 @@ type Scanner struct {
 	Worker int
 }
 
+// 执行结果
 type ExecResult struct {
 	IsOpen bool
 	Port   int
 }
 
+// 任务单元
 type Task struct {
 	Type string
 	Host string
@@ -30,16 +33,16 @@ func NewScanner(t string, p []int, h []string, url string, w int) *Scanner {
 		Type:   t,
 		Ports:  p,
 		Hosts:  h,
-		Url:    url,
-		Worker: w,
+		Url:    url, 
+		Worker: w,   // worker数量
 	}
 }
 
 func (s *Scanner) Run() {
-	taskChan := make(chan Task, 1024)
-	resultChan := make(chan ExecResult)
-	resultList := make([]ExecResult, 0)
-	startTime := time.Now()
+	taskChan := make(chan Task, 1024)    // 任务队列
+	resultChan := make(chan ExecResult)  // 结果接收队列
+	resultList := make([]ExecResult, 0)   // 结果汇总切片
+	startTime := time.Now()			
 
 	// 初始化协程池
 	for i := 0; i < s.Worker; i++ {
